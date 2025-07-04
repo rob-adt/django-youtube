bf746e1deced2fbba46e2b1410d5a956f98f5943
rob-adt <r.weeks0308@gmail.com>
2025-07-04 14:49:13 +0100

    did stuff

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from downloader import views
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", include("downloader.urls")),
+    path("admin/", admin.site.urls),
 ]
