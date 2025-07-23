99ea0c15429e555a71cb0f5ede8b7a9e465d3329
rob-adt <r.weeks0308@gmail.com>
2025-07-23 16:08:25 +0100

    .

--- downloader/views.py
+++ downloader/views.py
@@ -8,6 +8,9 @@ from .models import Video, Request
 from datetime import date
 import time
 import os
+from moviepy import CompositeAudioClip, VideoFileClip, AudioFileClip, CompositeVideoClip
+
+temp_folder = "tmp/"
 
 def index(request):
     if request.method == 'GET':
@@ -45,11 +48,31 @@ def download_video(request, video_pk):
     video = Video.objects.get(pk=video_pk)
     yt = YouTube(video.url, on_progress_callback=on_progress)
     title = yt.title
-    downlerd = yt.streams.get_highest_resolution()
-    downlerd.download()
+    # Get the highest resolution video stream (without audio)
+    video_stream = yt.streams.filter(file_extension="mp4", only_video=True,res=["1440p","1080p", "720p", "360p", "240p", "144p"]).order_by('resolution').desc().first()
+
+    # Get the highest quality audio stream
+    audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+
+    if not video_stream or not audio_stream:
+            raise Exception('No suitable video or audio streams found.')
+
+    # Log the resolution and audio quality
+    print(f"Downloading video in resolution: {video_stream.resolution}")
+    print(f"Downloading audio with bitrate: {audio_stream.abr}")
+
+    # Download video and audio streams
+    video_path = video_stream.download(output_path=temp_folder, filename="video.mp4", )
+    audio_path = audio_stream.download(output_path=temp_folder, filename="audio.mp3")  
+    videoclip = VideoFileClip("tmp/video.mp4")
+    audioclip = AudioFileClip("tmp/audio.mp3")
+
+    new_audioclip = CompositeAudioClip([audioclip])
+    videoclip.audio = new_audioclip
+    videoclip.write_videofile("tmp/combined_video.mp4") 
     filename = title + ".mp4"
     file_path = os.path.join(filename)
-    return FileResponse(open(file_path, 'rb'), filename=filename, as_attachment=True)
+    return FileResponse(open("tmp/combined_video.mp4", 'rb'), filename=filename, as_attachment=True)
 
 def download_audio(request, video_pk):
     video = Video.objects.get(pk=video_pk)
